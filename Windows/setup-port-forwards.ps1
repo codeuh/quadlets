df9108e38a499e284af42a6c68d413f05159a793
codeuh <codeuh@gmail.com>
2025-02-22 15:02:33 -0600

    Adjust setup-port-forwards.ps1 script to forward reverse proxy ssl ports.

--- Windows/setup-port-forwards.ps1
+++ Windows/setup-port-forwards.ps1
@@ -1,8 +1,2 @@
-netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=11434 connectaddress=127.0.0.1 connectport=11434
-netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=7474 connectaddress=127.0.0.1 connectport=7474
-netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=7687 connectaddress=127.0.0.1 connectport=7687
-netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=7070 connectaddress=127.0.0.1 connectport=7070
-netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=5000 connectaddress=127.0.0.1 connectport=5000
-netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=3000 connectaddress=127.0.0.1 connectport=3000
-netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=3001 connectaddress=127.0.0.1 connectport=3001
-netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=8502 connectaddress=127.0.0.1 connectport=8502
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=443 connectaddress=127.0.0.1 connectport=8443
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=8443 connectaddress=127.0.0.1 connectport=8443
