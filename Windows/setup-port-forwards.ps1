84b7022ae08fc6929f87cb65371e054fbad6f122
codeuh <codeuh@gmail.com>
2025-02-26 20:49:16 -0600

    Don't autostart / enable kokoro gpu container because it's in a crash restart loop after installing my 5090

--- Windows/setup-port-forwards.ps1
+++ Windows/setup-port-forwards.ps1
@@ -1,3 +1,2 @@
 netsh interface portproxy add v4tov4 listenport=443 listenaddress=0.0.0.0 connectport=8443 connectaddress=172.26.98.49
 netsh interface portproxy add v4tov4 listenport=8443 listenaddress=0.0.0.0 connectport=8443 connectaddress=172.26.98.49
-
