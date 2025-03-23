4a3d7e7c770727a57e64f5f7633d0ede9b91f9a8
codeuh <codeuh@gmail.com>
2025-03-22 19:35:09 -0500

    Update windows port forwards.

--- Windows/setup-port-forwards.ps1
+++ Windows/setup-port-forwards.ps1
@@ -1,2 +1,3 @@
 netsh interface portproxy add v4tov4 listenport=443 listenaddress=0.0.0.0 connectport=8443 connectaddress=172.26.98.49
 netsh interface portproxy add v4tov4 listenport=8443 listenaddress=0.0.0.0 connectport=8443 connectaddress=172.26.98.49
+netsh interface portproxy add v4tov4 listenport=11434 listenaddress=0.0.0.0 connectport=11434 connectaddress=172.26.98.49
