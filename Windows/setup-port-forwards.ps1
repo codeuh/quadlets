a4e48fbf05e329144ade67eee5cf10c5173adf5b
codeuh <codeuh@gmail.com>
2025-03-24 09:06:50 -0500

    Update windows port forwarding script.

--- Windows/setup-port-forwards.ps1
+++ Windows/setup-port-forwards.ps1
@@ -1,3 +1,3 @@
-netsh interface portproxy add v4tov4 listenport=443 listenaddress=0.0.0.0 connectport=8443 connectaddress=172.26.98.49
-netsh interface portproxy add v4tov4 listenport=8443 listenaddress=0.0.0.0 connectport=8443 connectaddress=172.26.98.49
 netsh interface portproxy add v4tov4 listenport=11434 listenaddress=0.0.0.0 connectport=11434 connectaddress=172.26.98.49
+netsh interface portproxy add v4tov4 listenport=8502 listenaddress=0.0.0.0 connectport=8502 connectaddress=172.26.98.49
+netsh interface portproxy add v4tov4 listenport=9882 listenaddress=0.0.0.0 connectport=9882 connectaddress=172.26.98.49
